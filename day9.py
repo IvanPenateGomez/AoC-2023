639114bfbbb8e2aec3fd4f8794f02234301e6e8a
Iván Peñate Gómez <90190159+IvanPenateGomez@users.noreply.github.com>
2023-12-09 06:44:21 +0100

    Rank 2527 XD , part 1 and 2

--- day9.py
+++ day9.py
@@ -1,18 +1,13 @@
 def part1(arr):
     diffArr = [arr[x + 1] - arr[x] for x in range(len(arr) - 1)]
     
-    if all(x == diffArr[0] for x in diffArr):
-        return arr[-1] + diffArr[0]
-    
-    return arr[-1] + part1(diffArr)
+    return arr[-1] + (diffArr[-1] if all(x == diffArr[0] for x in diffArr) else part1(diffArr))
+
 
 def part2(arr):
     diffArr = [arr[x + 1] - arr[x] for x in range(len(arr) - 1)]
     
-    if all(x == diffArr[0] for x in diffArr):
-        return arr[0] - diffArr[0]
-    
-    return arr[0] - part2(diffArr)
+    return arr[0] - (diffArr[0] if all(x == diffArr[0] for x in diffArr) else part2(diffArr))
 
 file = open("text.txt")
 print(sum(list(part1(list(map(int , line.strip().split()))) for line in file)))
